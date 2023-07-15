f862f9492e95ef23355266a1b0d765b1f687d858
Sachin Chaudhary <rcsachin003@gmail.com>
2023-07-15 13:31:52 +0530

    Starting Go

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Name"
+	fmt.Println(name)
+
+	var isbool bool = true
+	fmt.Println(isbool)
+
+	var interger int = 256
+	fmt.Println(interger)
+
+}
