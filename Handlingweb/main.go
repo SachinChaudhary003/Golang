e45698c34ce60f30de847a26b15437b056cb14dd
Sachin Chaudhary <rcsachin003@gmail.com>
2023-08-07 15:17:08 +0530

    webrequesthandling

---
+++ Handlingweb/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const url = "https://lco.dev"
+
+func main() {
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("response type %T \n ", response)
+	defer response.Body.Close()
+	databytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+	content := string(databytes)
+	fmt.Println(content)
+
+}
